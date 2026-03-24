900008a2bc6347730f55435e8c85bbcf3aa7274f
yuri4417 <109761467+yuri4417@users.noreply.github.com>
2026-03-23 22:20:09 -0300

    Corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 console.log("OK");
 const btn = document.querySelector(".add-btn");
-const submenu = document.querySelector(".submenu");
+const submenu = document.querySelector(".add-menu");
 
 btn.addEventListener("click", () => {
   submenu.classList.toggle("active");
